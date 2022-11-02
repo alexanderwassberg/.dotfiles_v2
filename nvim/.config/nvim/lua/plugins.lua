bd48066bb1c752f5106d1bba4ee63faeaf691943
Alexander Wassberg <alexanderwassberg@gmail.com>
2022-11-02 09:30:40 +0100

    Markdown Preview

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,62 +1,70 @@
 local status, packer = pcall(require, "packer")
 if (not status) then
-  print("Packer is not installed")
-  return
+    print("Packer is not installed")
+    return
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lua/plenary.nvim'
 
-  -- Find Files
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'brooth/far.vim'
-  use 'kyazdani42/nvim-tree.lua'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    -- Find Files
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-file-browser.nvim'
+    use 'brooth/far.vim'
+    use 'kyazdani42/nvim-tree.lua'
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  -- Status Bar
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
+    -- Status Bar
+    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 
-  -- LSP Stuff
-  use { 'neoclide/coc.nvim', branch = 'release' }
-  use 'neovim/nvim-lspconfig'
-  use 'onsails/lspkind-nvim'
+    -- LSP Stuff
+    use { 'neoclide/coc.nvim', branch = 'release' }
+    use 'neovim/nvim-lspconfig'
+    use 'onsails/lspkind-nvim'
 
-  -- Completion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
+    -- Completion
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
 
-  -- Git
-  use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim'
+    -- Git
+    use 'lewis6991/gitsigns.nvim'
+    use 'dinhhuy258/git.nvim'
 
-  -- Elm
-  use 'andys8/vim-elm-syntax'
-  use 'elmcast/elm-vim'
-  use 'preservim/tagbar'
-  use 'bitterjug/vim-tagbar-ctags-elm'
+    -- Elm
+    use 'andys8/vim-elm-syntax'
+    use 'elmcast/elm-vim'
+    use 'preservim/tagbar'
+    use 'bitterjug/vim-tagbar-ctags-elm'
 
-  -- Terminal
-  use 'voldikss/vim-floaterm'
+    -- Terminal
+    use 'voldikss/vim-floaterm'
 
-  -- Apperance
-  use 'kyazdani42/nvim-web-devicons'
-  use 'glepnir/dashboard-nvim'
-  use 'xiyaowong/nvim-transparent'
+    -- Apperance
+    use 'kyazdani42/nvim-web-devicons'
+    use 'glepnir/dashboard-nvim'
+    use 'xiyaowong/nvim-transparent'
 
-  -- Color Schemes
-  use 'rebelot/kanagawa.nvim'
-  use 'joshdick/onedark.vim'
-  use 'NLKNguyen/papercolor-theme'
-  use { 'embark-theme/vim', as = 'embark' }
-  use 'sainnhe/gruvbox-material'
-  use 'Shatur/neovim-ayu'
+    -- Color Schemes
+    use 'rebelot/kanagawa.nvim'
+    use 'joshdick/onedark.vim'
+    use 'NLKNguyen/papercolor-theme'
+    use { 'embark-theme/vim', as = 'embark' }
+    use 'sainnhe/gruvbox-material'
+    use 'Shatur/neovim-ayu'
+
+    -- Markdown Preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
 end)
