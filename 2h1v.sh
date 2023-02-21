d20698bc9693bd75d751838c087ef7db9d0f80a3
wassberg <alexander.wassberg@webbhuset.se>
2023-02-21 20:53:41 +0100

    Delete as many unused configs as possible

--- 2h1v.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 3440x1440 --pos 1920x705 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 5360x0 --rotate left --output HDMI-A-0 --off --output HDMI-A-1 --mode 1920x1080 --pos 0x788 --rotate normal --output DVI-D-0 --off
